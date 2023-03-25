549c699518fea46f46faf4f0043d5279133a3bd2
Lazar Djurovic <lazardjurovic59@gmail.com>
2023-03-25 17:35:45 +0100

    forwarding unit finished

--- RISC-V.srcs/sources_1/new/forwarding_unit.vhd
+++ RISC-V.srcs/sources_1/new/forwarding_unit.vhd
@@ -24,3 +24,52 @@ entity forwarding_unit is
 		branch_forward_b_o : out std_logic
 	);
 end entity;
+
+architecture behav of forwarding_unit is
+begin
+
+    process(rs1_address_id_i,rs2_address_id_i,rs1_address_ex_i,rs2_address_ex_i,rd_we_mem_i,rd_address_mem_i,rd_we_wb_i,rd_address_wb_i)
+    begin
+            
+        if(rd_we_mem_i = '1' and rd_address_mem_i /= "0000") then -- forwarding from mem phase
+            if(rs1_address_ex_i = rd_address_mem_i) then
+                alu_forward_a_o <= "10";
+            end if; 
+            
+            if(rs2_address_ex_i = rd_address_mem_i) then
+                alu_forward_b_o <= "10";
+            end if;   
+            
+        elsif(rd_we_wb_i = '1' and rd_address_wb_i /= "0000") then  -- forwarding from wb phase
+            if(rs1_address_ex_i = rd_address_wb_i) then
+                alu_forward_a_o <= "01";
+            end if; 
+            
+            if(rs2_address_ex_i = rd_address_wb_i) then
+                alu_forward_b_o <= "01";
+            end if;
+            
+        else
+            alu_forward_a_o <= "00";
+            alu_forward_b_o <= "00";   
+        end if;
+        
+        
+    -- Branch forwarding   
+    if(rd_we_mem_i = '1' and rd_address_mem_i /= "0000") then
+        
+        if(rs1_address_id_i = rd_address_mem_i) then
+            branch_forward_a_o <= '1';
+        end if;
+        
+        if(rs2_address_id_i = rd_address_mem_i) then
+            branch_forward_b_o <= '1';
+        end if;
+    else
+        branch_forward_a_o <= '0';
+        branch_forward_b_o <= '0';    
+    end if;        
+        
+    end process;
+
+end behav;
