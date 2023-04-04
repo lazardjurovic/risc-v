e17d2caf40f44ab228717b4727049d2709b7f84f
Lazar Djurovic <lazardjurovic59@gmail.com>
2023-04-04 19:31:00 +0200

    fixed BRAMsesitivity list

--- RISC-V.srcs/sources_1/new/BRAM.vhd
+++ RISC-V.srcs/sources_1/new/BRAM.vhd
@@ -61,7 +61,7 @@ begin
 		end if;
 	end process;
 	-- asinhrono citanje
-	process (en_a_i, en_b_i, addr_a_i, addr_b_i)
+	process (en_a_i, en_b_i, addr_a_i, addr_b_i,ram_s)
 		begin
 			if (en_a_i = '1') then
 				data_a_o(31 downto 24) <= ram_s(to_integer(unsigned(addr_a_i) + 3));
